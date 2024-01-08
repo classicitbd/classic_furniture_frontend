8d12fcc939e2882272abf74053c8d6d4affcd1b1
Moloy das <classicitdevteam@gmail.com>
2024-01-08 13:16:59 +0600

    slider 70% done

--- src/components/frontend/ui/slider/Slider.jsx
+++ src/components/frontend/ui/slider/Slider.jsx
@@ -5,6 +5,7 @@ import {
   A11y,
   Autoplay,
   Keyboard,
+  Parallax,
 } from "swiper/modules";
 import { FcNext, FcPrevious } from "react-icons/fc";
 
@@ -12,6 +13,8 @@ import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
 import "swiper/css";
+import "swiper/css/bundle";
+// import "swiper/css/keyboard";
 import { sliderData } from "../../../../data/slider-data";
 
 const Slider = () => {
@@ -19,8 +22,10 @@ const Slider = () => {
     <div className="main-container">
       <Swiper
         // install Swiper modules
-        modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay, Keyboard]}
+        modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay, Keyboard, Parallax]}
         slidesPerView={1}
+        speed={1000}
+        parallax={true}
         navigation={{
           nextEl: ".next",
           prevEl: ".prev",
@@ -34,7 +39,7 @@ const Slider = () => {
         onSwiper={() => {}}
         onSlideChange={() => {}}
       >
-        <div className="relative">
+        <div className="relative" data-swiper-parallax-duration="2000">
           {sliderData.map((slider) => (
             <SwiperSlide key={slider.id}>
               <img
@@ -46,14 +51,14 @@ const Slider = () => {
           ))}
         </div>
         <div className="">
-          <button className="prev w-10 h-10 absolute top-[50%] left-[10px] z-10">
+          <button className="prev w-10 h-10 absolute top-[50%] left-[10px] z-10 bg-gray-600 bg-opacity-0 hover:bg-opacity-40 rounded flex items-center justify-center transition-all duration-300">
             <span>
-              <FcPrevious className="text-5xl" />
+              <FcPrevious className="text-5xl p-1" />
             </span>
           </button>
-          <button className="next w-10 h-10 absolute top-[50%] right-[10px] z-10">
+          <button className="next w-10 h-10 absolute top-[50%] right-[10px] z-10 bg-gray-600 bg-opacity-0 hover:bg-opacity-40 rounded flex items-center justify-center transition-all duration-300">
             <span>
-              <FcNext className="text-5xl" />
+              <FcNext className="text-5xl p-1 font-light" />
             </span>
           </button>
         </div>
