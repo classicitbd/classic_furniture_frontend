1ca5e48625494c4ce8fd267d9dabcdba24f4e20a
Moloy das <classicitdevteam@gmail.com>
2024-01-08 15:39:25 +0600

    category section create is done

--- src/components/frontend/ui/category/Category.jsx
+++ src/components/frontend/ui/category/Category.jsx
@@ -1,4 +1,13 @@
+import { Link } from "react-router-dom";
+import { categoryData } from "../../../../data/category-data";
+import { useState } from "react";
+
 const Category = () => {
+  const [hoveredCategory, setHoveredCategory] = useState(null);
+
+  const handleCategoryHover = (categoryId) => {
+    setHoveredCategory(categoryId);
+  };
   return (
     <section>
       <article className="text-wrap py-[30px]">
@@ -7,6 +16,27 @@ const Category = () => {
         </h2>
         <p className="text-center">Step into the fusion of fine materials</p>
       </article>
+      <nav className="py-[40px] bg-[#000000]">
+        <ul className="flex flex-col md:flex-row items-center justify-between gap-[10px] md:gap-[20px] text-lg md:text-xl lga:text-2xl max-w-4xl px-[20px] mx-auto">
+          {categoryData.map((item, i) => (
+            <li key={item.id}>
+              <Link
+                className={`${
+                  categoryData.length === item.id && "text-error-300"
+                } uppercase text-white`}
+                style={{
+                  opacity: hoveredCategory === i ? 1 : 0.75,
+                  transition: "opacity 0.2s ease-in-out", // Optional: Add a transition for a smoother effect
+                }}
+                onMouseEnter={() => setHoveredCategory(i)}
+                onMouseLeave={() => setHoveredCategory(null)}
+              >
+                {item.category}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </section>
   );
 };
--- src/pages/frontend/home/Home.jsx
+++ src/pages/frontend/home/Home.jsx
@@ -1,10 +1,10 @@
 import Category from "../../../components/frontend/ui/category/Category";
-import Slider from "../../../components/frontend/ui/slider/Slider";
+// import Slider from "../../../components/frontend/ui/slider/Slider";
 
 const Home = () => {
   return (
     <main className="">
-      <Slider />
+      {/* <Slider /> */}
       <Category />
     </main>
   );
