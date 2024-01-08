bdd2c13483d74bcc1c279b800eccdc99b53004c3
Moloy das <classicitdevteam@gmail.com>
2024-01-08 15:08:14 +0600

    category section 50% is done

---
+++ src/components/frontend/ui/category/Category.jsx
@@ -0,0 +1,14 @@
+const Category = () => {
+  return (
+    <section>
+      <article className="text-wrap py-[30px]">
+        <h2 className="text-lg md:text-xl lg:text-2xl font-semibold text-center align-baseline px-[10px]">
+          LUXURY HANDCRAFTED FOOTWEAR & FASHION RETAIL
+        </h2>
+        <p className="text-center">Step into the fusion of fine materials</p>
+      </article>
+    </section>
+  );
+};
+
+export default Category;
--- src/pages/frontend/home/Home.jsx
+++ src/pages/frontend/home/Home.jsx
@@ -1,9 +1,11 @@
+import Category from "../../../components/frontend/ui/category/Category";
 import Slider from "../../../components/frontend/ui/slider/Slider";
 
 const Home = () => {
   return (
     <main className="">
       <Slider />
+      <Category />
     </main>
   );
 };
