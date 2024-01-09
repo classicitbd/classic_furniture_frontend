b8918436e20d2bfb9a30a10c1473d175fb28ad2d
Moloy das <classicitdevteam@gmail.com>
2024-01-09 14:50:19 +0600

    collection top category 70% is done

--- src/components/frontend/ui/collectionCategory/CollectionTopCategory.jsx
+++ src/components/frontend/ui/collectionCategory/CollectionTopCategory.jsx
@@ -3,15 +3,20 @@ import { collectionData } from "../../../../data/collection-data";
 
 const CollectionTopCategory = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-5 grid-rows-1 md:grid-rows-2">
+    <div className="grid grid-cols-1 md:grid-cols-7 grid-rows-1 md:grid-rows-2">
       {/* First Section */}
-      <div className="relative group overflow-hidden md:col-span-2 md:row-span-2">
-        <Link to={`/all?collection=FW302`}></Link>
+      <div className="relative group overflow-hidden md:col-span-3 md:row-span-2">
+        <Link to={`/all?collection=FW302`} className="block w-full h-full">
+          <video className="w-full h-full object-cover" autoPlay muted loop>
+            <source src="/assets/video/shoe.mp4" type="video/mp4" />
+            {/* Your browser does not support the video tag. */}
+          </video>
+        </Link>
       </div>
 
       {/* Second Section */}
-      <div className="relative group overflow-hidden md:col-span-3 row-span-1 flex flex-col items-center justify-center">
-        <article className="text-center uppercase leading-tight">
+      <div className="relative group overflow-hidden md:col-span-4 row-span-1 flex flex-col items-center justify-center space-y-2 py-20 md:py-0">
+        <article className="text-center uppercase leading-tight space-y-1">
           <h1 className="text-2xl md:text-5xl font-semibold">Regular Sale</h1>
           <p>Select Styles On Sale</p>
         </article>
@@ -26,12 +31,12 @@ const CollectionTopCategory = () => {
       {/* Third Section */}
       {collectionData.slice(3, 4).map((product) => (
         <div
-          className="relative group overflow-hidden md:col-span-3 row-span-1"
+          className="relative group overflow-hidden md:col-span-4 row-span-1"
           key={product?.id}
         >
           <Link to={`/all?sub_category=${product?.slug}`}>
             <img
-              className="transition-transform transform duration-1500 ease-in-out group-hover:scale-110 object-cover h-[400px] w-full"
+              className="transition-transform transform duration-1500 ease-in-out group-hover:scale-110 object-cover md:h-[400px] w-full"
               src={product?.thumbnail}
               alt={product?.title}
             />
