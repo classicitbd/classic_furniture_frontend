0696a54cbb8d16aab9383d24a6d1312651154f89
Moloy das <classicitdevteam@gmail.com>
2024-01-09 19:04:33 +0600

    product details page create 91% is done

--- src/components/frontend/ui/details/ProductDetails.jsx
+++ src/components/frontend/ui/details/ProductDetails.jsx
@@ -15,7 +15,7 @@ import Breadcrumb from "../../../common/breadCrumb/BreadCrumb";
 
 const ProductDetails = () => {
   return (
-    <section className="grid grid-cols-1 lg:grid-cols-2 h-[50vh] md:h-[100vh] px-2">
+    <section className="grid grid-cols-1 lg:grid-cols-2 px-2">
       <Swiper
         modules={[Parallax, Mousewheel, Pagination, Autoplay, Keyboard]}
         direction={"vertical"}
@@ -31,6 +31,7 @@ const ProductDetails = () => {
         pagination={{ clickable: true }}
         onSwiper={() => {}}
         onSlideChange={() => {}}
+        className="h-[50vh] md:h-[100vh]"
       >
         {singleProduct?.images?.map((index) => (
           <SwiperSlide key={index} className="border">
@@ -42,9 +43,9 @@ const ProductDetails = () => {
           </SwiperSlide>
         ))}
       </Swiper>
-      <div className="px-10 py-5">
+      <div className="md:px-10 py-5">
         <Breadcrumb product={singleProduct} />
-        <div className="px-4">
+        <div className="md:px-4">
           <article className="space-y-5 mt-8 border-bgray-500">
             <h1 className="text-xl font-semibold">{singleProduct.title}</h1>
             <p className="text-xl font-normal">BDT {singleProduct?.price}.00</p>
@@ -60,7 +61,9 @@ const ProductDetails = () => {
 
           <div className="">
             <p className="py-7 text-xl font-medium">Products Details</p>
-            <div dangerouslySetInnerHTML={{__html: singleProduct.description}}></div>
+            <div
+              dangerouslySetInnerHTML={{ __html: singleProduct.description }}
+            ></div>
           </div>
         </div>
       </div>
