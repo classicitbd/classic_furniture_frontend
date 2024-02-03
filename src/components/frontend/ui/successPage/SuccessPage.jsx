1aa20c262e5173eadf888cce170e87c83fbe1c7e
Moloy das <classicitdevteam@gmail.com>
2024-02-03 18:32:51 +0600

    successfull page is updated

--- src/components/frontend/ui/successPage/SuccessPage.jsx
+++ src/components/frontend/ui/successPage/SuccessPage.jsx
@@ -1,19 +1,14 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { cartKey } from "../../../../constants/cartKey";
-import { useEffect } from "react";
 
 const SuccessPage = () => {
-  // const { tranId } = useParams();
+  const navigate = useNavigate();
 
-  useEffect(() => {
+  const handleNavigate = () => {
     localStorage.removeItem(cartKey);
-    const timerId = setTimeout(() => {
-      window.location.reload();
-    }, 10);
-
-    // Cleanup the timer to avoid memory leaks
-    return () => clearTimeout(timerId);
-  }, []);
+    navigate("/user-profile");
+    window.location.reload();
+  };
 
   return (
     <div>
@@ -24,12 +19,12 @@ const SuccessPage = () => {
         <p className="text-lg text-gray-600 mb-8">
           Thank you for your purchase.
         </p>
-        <Link
-          to="/user-profile"
+        <button
           className="bg-blue-500 text-white px-4 py-2 rounded-md"
+          onClick={() => handleNavigate()}
         >
           See your Order
-        </Link>
+        </button>
       </div>
     </div>
   );
