8c69cb4676caa532978178ba065a453cc0180198
Hasiful Islam <nazmulbhuyian000@gmail.com>
2024-02-05 14:22:09 +0600

    dashboard name change

--- src/layout/DashboardLayout.jsx
+++ src/layout/DashboardLayout.jsx
@@ -1,12 +1,12 @@
 import { Outlet } from "react-router-dom";
 import SideNavBar from "../shared/sideNavBar/SideNavBar";
-import DashboardNavbar from "../shared/Navbar/dashboardNavbar";
+import DashboardNavbars from "../shared/Navbar/DashboardNavbars";
 
 const DashboardLayout = () => {
     return (
         <div className="min-h-screen w-full h-full bg-white">
             <div className="fixed top-0 left-0 w-full z-50 bg-black text-white">
-                <DashboardNavbar />
+                <DashboardNavbars />
             </div>
             <div className="grid lg:grid-cols-12 grid-cols-4">
                 <div className="lg:col-span-2">
