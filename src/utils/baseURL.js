f740981417036dd867ccc0894134a96d98a9d193
nazmulbhuyian <nazmulbhuyian000@gmail.com>
2024-04-20 18:56:24 +0600

    done

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,2 +1,2 @@
 // export const BASE_URL = "http://localhost:5000/api/v1";
-export const BASE_URL = "https://seashell-app-8o3sp.ondigitalocean.app/api/v1";
+export const BASE_URL = "https://walrus-app-btp85.ondigitalocean.app/api/v1";
