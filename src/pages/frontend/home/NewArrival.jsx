584a8bd10e494c2ddb6b9a5aec98362f13c642be
iliasrony21 <iliasronyweb21@gmail.com>
2024-05-09 11:17:47 +0600

    New arrival product and slider get api create

--- src/pages/frontend/home/NewArrival.jsx
+++ src/pages/frontend/home/NewArrival.jsx
@@ -1,148 +1,15 @@
-import {
-  FaAngleDown,
-  FaAngleRight,
-  FaShoppingCart,
-  FaStar,
-  FaStarHalfAlt,
-} from "react-icons/fa";
-import { IoCartOutline } from "react-icons/io5";
+import { FaShoppingCart } from "react-icons/fa";
 import { useEffect, useState } from "react";
-import Header from "../../../shared/header/Header";
+import { useGetProductQuery } from "../../../redux/feature/product/productApi";
+import MiniSpinner from "../../../shared/loader/MiniSpinner";
 
 export default function NewArrival() {
-  const {data: products
-  const [showAll, setShowAll] = useState(false);
+  const { data: products, isLoading } = useGetProductQuery(undefined, {
+    refetchOnMountOrArgChange: true,
+    pollingInterval: 3000,
+  });
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-  const initialDisplayCount = 10;
-  const [displayedProducts, setdisplayedProducts] = useState([]);
-  const [showMoreCount, setShowMoreCount] = useState(10);
-  const [cartQuantity, setCartQuantity] = useState(0);
-  const productSale = [
-    {
-      id: 1,
-      product_id: "P001",
-      title: "Horitoki Special all",
-      new_price: 49.99,
-      old_price: 59.99,
-      image: "https://i.ibb.co/hDJMYkx/bedroom.jpg",
-      rating: 4.5,
-    },
-    {
-      id: 2,
-      product_id: "P002",
-      title: "Traditional Red and Gold",
-      new_price: 39.99,
-      old_price: 49.99,
-      image: "https://i.ibb.co/L8PDJSh/hospital.jpg",
-      rating: 4.2,
-    },
-    {
-      id: 3,
-      product_id: "P003",
-      title: "Traditional Red and Gold",
-      new_price: 79.99,
-      old_price: 99.99,
-      image: "https://i.ibb.co/Dp5dSG9/living.jpg",
-      rating: 4.7,
-    },
-    {
-      id: 4,
-      product_id: "P004",
-      title: "Stylish Black ",
-      new_price: 99.99,
-      old_price: 69.99,
-      image: "https://i.ibb.co/GpxJwJf/office.jpg",
-      rating: 4.4,
-    },
-    {
-      id: 5,
-      product_id: "P005",
-      title: "Vintage Denim ",
-      new_price: 29.99,
-      old_price: 39.99,
-      image: "https://i.ibb.co/hDJMYkx/bedroom.jpg",
-      rating: 4.0,
-    },
-    {
-      id: 6,
-      product_id: "P006",
-      title: "Casual Striped",
-      new_price: 19.99,
-      old_price: 24.99,
-      image: "https://i.ibb.co/GPrR0Xj/dining.jpg",
-      rating: 4.3,
-    },
-    {
-      id: 7,
-      product_id: "P007",
-      title: "Bohemian Style",
-      new_price: 34.99,
-      old_price: 44.99,
-      image: "https://i.ibb.co/GpxJwJf/office.jpg",
-      rating: 4.6,
-    },
-    {
-      id: 8,
-      product_id: "P008",
-      title: "Classic Aviator ",
-      new_price: 14.99,
-      old_price: 19.99,
-      image: "https://i.ibb.co/Dp5dSG9/living.jpg",
-      rating: 4.8,
-    },
-    {
-      id: 9,
-      product_id: "P009",
-      title: "Chic Floral",
-      new_price: 9.99,
-      old_price: 6.99,
-      image: "https://i.ibb.co/L8PDJSh/hospital.jpg",
-      rating: 4.1,
-    },
-    {
-      id: 10,
-      product_id: "P010",
-      title: "Retro Style Leather",
-      new_price: 54.99,
-      old_price: 64.99,
-      image: "https://i.ibb.co/vHngdhG/doors.jpg",
-      rating: 4.9,
-    },
-    {
-      id: 15,
-      product_id: "P009",
-      title: "Chic Floral",
-      new_price: 9.99,
-      old_price: 6.99,
-      image: "https://i.ibb.co/GPrR0Xj/dining.jpg",
-      rating: 4.1,
-    },
-    {
-      id: 16,
-      product_id: "P010",
-      title: "Retro Style Leather",
-      new_price: 54.99,
-      old_price: 64.99,
-      image: "https://i.ibb.co/hDJMYkx/bedroom.jpg",
-      rating: 4.9,
-    },
-  ];
-
-  useEffect(() => {
-    setdisplayedProducts(productSale?.slice(0, initialDisplayCount));
-  }, []);
-
-  const handleViewAll = () => {
-    setShowAll(!showAll);
-    setdisplayedProducts(productSale.slice(0, 20));
-    setShowMoreCount(20);
-  };
-
-  const handleShowMore = () => {
-    const newCount = showMoreCount + 20;
-    setdisplayedProducts(productSale.slice(0, newCount));
-    setShowMoreCount(newCount);
-  };
+  // const [cartQuantity, setCartQuantity] = useState(0);
 
   useEffect(() => {
     const handleResize = () => {
@@ -162,12 +29,18 @@ export default function NewArrival() {
   if (windowWidth >= 1024) gridCols = "lg:grid-cols-4";
   if (windowWidth >= 1280) gridCols = "2xl:grid-cols-5";
 
-  const handleAddToCart = () => {
-    setCartQuantity((prevQuantity) => prevQuantity + 1);
-  };
+  // const handleAddToCart = () => {
+  //   setCartQuantity((prevQuantity) => prevQuantity + 1);
+  // };
 
-  console.log("cartQuantity", cartQuantity);
+  // console.log("cartQuantity", cartQuantity);
 
+  if (isLoading)
+    return (
+      <div>
+        <MiniSpinner />
+      </div>
+    );
   return (
     <>
       <div className="es_container mx-auto md:px-20 xl:px-0 px-5 pt-10">
@@ -176,110 +49,112 @@ export default function NewArrival() {
             <h1 className="text-xl my-6 font-semibold text-[#008140]">
               New Arival
             </h1>
-            <div
-              onClick={handleViewAll}
-              className="cursor-pointer font-medium flex items-center gap-1"
-            >
-              <span>View All</span>{" "}
-              {showAll ? <FaAngleDown /> : <FaAngleRight />}
-            </div>
           </div>
         </div>
         <div className={`grid gap-4 ${gridCols}`}>
-          {displayedProducts.map((product) => (
-            <div key={product.id} className="group">
-              <span>
-                <div
-                  title="View Details"
-                  className="border border-transparent group-hover:border-black transition-all duration-500 rounded-md group-hover:scale-100 shadow"
-                >
-                  <div className="w-full relative">
-                    <img
-                      src={product?.image}
-                      alt={product?.title}
-                      className="w-full h-[120px] md:h-[180px] rounded-lg "
-                    />
+          {products?.data?.map((product) => (
+            <div key={product?._id} className="group">
+              <div
+                title="View Details"
+                className="border border-transparent group-hover:border-[#008140] transition-all duration-500 rounded-md group-hover:scale-100 shadow"
+              >
+                <div className="w-full relative">
+                  <img
+                    src={product?.product_thumbnail}
+                    alt="Product Image"
+                    className="w-full rounded-t-lg"
+                  />
+                  {product?.product_discount_price && (
                     <div className="bg-red-600 text-white inline px-1 rounded text-[12px] absolute top-0 right-0 m-2">
-                      OFF 100%
+                      OFF{" "}
+                      {product?.product_price - product?.product_discount_price}
+                      TK
                     </div>
-                  </div>
-                  <div className=" px-2 py-5">
-                    <div className="product_rating flex">
-                      {[...Array(Math.floor(product.rating))].map(
-                        (_, index) => (
-                          <FaStar key={index} className="text-yellowColor" />
-                        )
-                      )}
-                      {product.rating % 1 >= 0.5 && (
-                        <FaStarHalfAlt className="text-yellowColor" />
-                      )}
-                    </div>
-
-                    <div className="product_title mt-2">
-                      <p
-                        title={product.title}
-                        className={`text-[14px] text-[#041826] leading-5 font-medium group-hover:text-ftPrimaryColor duration-200 transition-all ${
-                          windowWidth < 640
-                            ? "max-w-[10rem] overflow-hidden whitespace-nowrap overflow-ellipsis"
-                            : ""
-                        }`}
-                      >
-                        {windowWidth >= 1024
-                          ? product.title.length > 30
-                            ? `${product.title.slice(0, 30)}...`
-                            : product.title
-                          : product.title.length > 23
-                          ? `${product.title.slice(0, 23)}...`
-                          : product.title}
-                      </p>
-                    </div>
-
-                    <div
-                      className={`product_price_inner flex items-center gap-2 py-2`}
-                    >
-                      <strong className="offer_price text-ftPrimaryColor text-[16px] font-bold ">
-                        ৳ {product.new_price}
-                      </strong>
-                      <span className="old_price line-through text-[14px] text-[#0f172a99] font-medium">
-                        ৳ {product.old_price}
+                  )}
+                </div>
+                {/* Product Details */}
+                <div className="px-3 pt-3 pb-2">
+                  {/* Product Prices */}
+                  <div className="product_price_inner flex items-center gap-2 py-2">
+                    <strong className="offer_price text-[#FF0000] text-[16px] font-bold">
+                      ৳{" "}
+                      {product?.product_discount_price
+                        ? product?.product_discount_price
+                        : product?.product_price}
+                    </strong>
+                    {product?.product_discount_price && (
+                      <span className="old_price line-through text-[12px] text-[#0f172a99] font-medium">
+                        ৳ {product?.product_price}
                       </span>
+                    )}
+                  </div>
+                  {product?.product_quantity ? (
+                    <div className=" text-sm text-gray-500">
+                      <p> In-Stock</p>
                     </div>
+                  ) : (
+                    <div className=" text-xs text-gray-500">
+                      <p> Out Of Stock</p>
+                    </div>
+                  )}
+                  {/* Rating Stars */}
+                  {/* <div className="product_rating flex">
+                            {[...Array(Math.floor(product.rating))].map(
+                              (_, index) => (
+                                <FaStar
+                                  key={index}
+                                  className="text-yellowColor"
+                                />
+                              )
+                            )}
+                            {product.rating % 1 >= 0.5 && (
+                              <FaStarHalfAlt className="text-yellowColor" />
+                            )}
+                          </div> */}
+                  {/* Product Title */}
+                  <div className="product_title mt-2">
+                    <p
+                      title={product?.product_name}
+                      className={`text-[14px] text-[#041826] leading-5 font-medium group-hover:text-ftPrimaryColor duration-200 transition-all ${
+                        window.innerWidth < 640
+                          ? "max-w-[10rem] overflow-hidden whitespace-nowrap overflow-ellipsis"
+                          : ""
+                      }`}
+                    >
+                      {window.innerWidth >= 1024
+                        ? product.product_name.length > 30
+                          ? `${product.product_name.slice(0, 30)}...`
+                          : product.product_name
+                        : product.product_name.length > 23
+                        ? `${product.product_name.slice(0, 23)}...`
+                        : product.product_name}
+                    </p>
+                  </div>
 
-                    <div className="flex w-full items-center justify-between gap-4 text-[#008140] pe-8">
-                      <button
-                        type="button"
-                        title="Buy Now"
-                        className="bg-ftPrimaryColor py-2  rounded text-center font-semibold text-[16px]"
-                        icon={IoCartOutline}
-                      >
-                        Buy Now
-                      </button>
-                      <FaShoppingCart
-                        onClick={handleAddToCart}
-                        title="Add To cart"
-                        className="text-xl"
-                      />
-                    </div>
+                  {/* Buy Now Button */}
+                  <div className="flex w-full items-center  gap-2 text-[#008140] cursor-pointer">
+                    <FaShoppingCart
+                      // onClick={() => handleAddToCart(product)}
+                      title="Add To cart"
+                      className="text-lg "
+                    />
+                    <button
+                      type="button"
+                      title="Buy Now"
+                      className="bg-ftPrimaryColor py-2 rounded   text-center font-semibold text-[16px]"
+                      // onClick={() => handleAddToCart(product)}
+                    >
+                      Buy Now
+                    </button>
                   </div>
                 </div>
-              </span>
+              </div>
             </div>
           ))}
         </div>
-        <div className="hidden">
+        {/* <div className="hidden">
           <Header cartQuantity={cartQuantity} />
-        </div>
-        {displayedProducts.length >= 20 &&
-          displayedProducts.length < productSale.length && (
-            <div className="text-center ">
-              <button
-                onClick={handleShowMore}
-                className="mt-4 bg-[#008140] hover:bg-[#183f2c] text-white font-bold py-2 px-4 rounded shadow focus:outline-none focus:ring-2 focus:ring-blue-200"
-              >
-                Show More
-              </button>
-            </div>
-          )}
+        </div> */}
       </div>
     </>
   );
--- src/redux/feature/banner/bannerApi.js
+++ src/redux/feature/banner/bannerApi.js
@@ -1,4 +1,3 @@
-
 import { api } from "../../api/apiSlice";
 import { authKey } from "../../../constants/storageKey";
 import { getCookie } from "../../../utils/cookie-storage";
@@ -6,50 +5,59 @@ import { getCookie } from "../../../utils/cookie-storage";
 const token = getCookie(authKey);
 
 export const bannerApi = api.injectEndpoints({
-  
   endpoints: (build) => ({
+    //  get banner
+    getBanner: build.query({
+      query: () => ({
+        url: `/banner`,
+        method: "GET",
+      }),
+      providesTags: ["banner"],
+    }),
     //add banner
     addBanner: build.mutation({
       query: (data) => ({
         url: `/banner`,
         method: "POST",
         headers: {
-        authorization: `Bearer ${token}`,
+          authorization: `Bearer ${token}`,
         },
-          body: data,
-        }),
-        invalidatesTags: ['banner'],
+        body: data,
       }),
- 
+      invalidatesTags: ["banner"],
+    }),
 
     //delete Banner
     deleteBanner: build.mutation({
-        query: (data) => ({
-            url: `/banner`,
-          method: "DELETE",
-          headers: {
+      query: (data) => ({
+        url: `/banner`,
+        method: "DELETE",
+        headers: {
           authorization: `Bearer ${token}`,
-          },
-          body: data,
-          
-        }),
-        invalidatesTags: ['banner'],
-      }),
-
-      //update Banner
-    updateBanner: build.mutation({
-        query: (data) => ({
-          url: `/banner`,
-          method: "PATCH",
-          headers: {
-        authorization: `Bearer ${token}`,
         },
-          body: data,
-        }),
-        invalidatesTags: ['banner'],
+        body: data,
       }),
+      invalidatesTags: ["banner"],
+    }),
 
+    //update Banner
+    updateBanner: build.mutation({
+      query: (data) => ({
+        url: `/banner`,
+        method: "PATCH",
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+        body: data,
+      }),
+      invalidatesTags: ["banner"],
+    }),
   }),
 });
 
-export const { useAddBannerMutation, useDeleteBannerMutation, useUpdateBannerMutation } = bannerApi;
+export const {
+  useGetBannerQuery,
+  useAddBannerMutation,
+  useDeleteBannerMutation,
+  useUpdateBannerMutation,
+} = bannerApi;
