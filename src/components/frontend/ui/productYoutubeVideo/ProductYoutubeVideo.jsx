f5af6c83f4f3d1e07f09c11d04d4404952a704a1
Saifulla Jubair <saifullajubair6@gmail.com>
2024-05-12 15:28:59 +0600

    fetch video data

--- src/components/frontend/ui/productYoutubeVideo/ProductYoutubeVideo.jsx
+++ src/components/frontend/ui/productYoutubeVideo/ProductYoutubeVideo.jsx
@@ -1,3 +1,17 @@
+import { useGetVideoQuery } from "../../../../redux/feature/video/videoApi";
+import MiniSpinner from "../../../../shared/loader/MiniSpinner";
+
 export default function ProductYoutubeVideo() {
+  const { data: videos, isLoading } = useGetVideoQuery(undefined, {
+    refetchOnMountOrArgChange: true,
+    pollingInterval: 60000,
+  });
+  if (isLoading)
+    return (
+      <div>
+        <MiniSpinner />
+      </div>
+    );
+  console.log(videos);
   return <div></div>;
 }
