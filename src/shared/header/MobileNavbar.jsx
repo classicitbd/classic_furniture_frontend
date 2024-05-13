b124b81af3ff7ec54d688959bd9b21ffa1abff99
Saifulla Jubair <saifullajubair6@gmail.com>
2024-05-13 16:17:50 +0600

    Mobile navbar fix

--- src/shared/header/MobileNavbar.jsx
+++ src/shared/header/MobileNavbar.jsx
@@ -1,23 +1,32 @@
-import { FaHome, FaShoppingCart, FaYoutube, FaUser } from "react-icons/fa";
+import { FaRegUserCircle } from "react-icons/fa";
+import { BiCategoryAlt } from "react-icons/bi";
+import { RiHome6Line } from "react-icons/ri";
+import { BsCart2 } from "react-icons/bs";
 import { Link } from "react-router-dom";
+import { AiOutlineYoutube } from "react-icons/ai";
 
 export default function MobileNavbar() {
   return (
-    <div className="fixed bottom-0 left-0 right-0 bg-white shadow-lg flex justify-around py-3 lg:hidden mb-2 rounded-lg mx-4">
+    <div className="fixed bottom-0 left-0 right-0 bg-white shadow-lg flex justify-around py-3 lg:hidden mb-2 rounded-lg mx-4 h-12">
       <Link to="/" className="text-black">
-        <FaHome size={24} />
+        <RiHome6Line size={24} />
       </Link>
       <Link to="/products" className="text-black">
-        <FaShoppingCart size={24} />
+        <BiCategoryAlt size={24} />
       </Link>
-      <Link to="/cart" className="bg-primaryDeepColor rounded-full text-white">
-        <FaShoppingCart size={24} />
+      <Link to="/cart">
+        <div className="bg-primaryLightColor  w-12 h-12 bottom-3 flex items-center  justify-center rounded-full relative text-white shadow-lg shadow-slate-600">
+          <BsCart2 size={24} />{" "}
+          <sup className="text-white w-5 h-5 flex items-center justify-center absolute -top-1 -right-1 bg-red-600 rounded-full">
+            2
+          </sup>
+        </div>
       </Link>
       <Link to="/youtube" className="text-black">
-        <FaYoutube size={24} />
+        <AiOutlineYoutube size={24} />
       </Link>
       <Link to="/profile" className="text-black">
-        <FaUser size={24} />
+        <FaRegUserCircle size={24} />
       </Link>
     </div>
   );
