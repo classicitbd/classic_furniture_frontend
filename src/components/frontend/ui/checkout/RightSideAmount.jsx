c2815f3e0076c1f52a5adf9c025d255469cc6380
Saifulla Jubair <saifullajubair6@gmail.com>
2024-05-15 16:06:49 +0600

    checkout page right side component create

---
+++ src/components/frontend/ui/checkout/RightSideAmount.jsx
@@ -0,0 +1,50 @@
+export default function RightSideAmount({
+  subTotal,
+  handleBack,
+  handleContinue,
+}) {
+  return (
+    <div className="w-full md:w-[32%] pt-16  ">
+      <div className=" rounded-lg border shadow bg-white p-4">
+        <div className="md:px-6   py-3 ">
+          <div className="pb-1.5 flex justify-between items-center">
+            <p>Item total</p>
+            <p>৳ {subTotal}</p>
+          </div>
+          <div className="flex justify-between py-2 items-center">
+            {" "}
+            <p>Delivery fee</p>
+            <p>৳ 100</p>
+          </div>
+          <hr />
+          <div className="flex justify-between py-2 items-center">
+            {" "}
+            <p>Grand total</p>
+            <p>৳ {subTotal}</p>
+          </div>
+
+          <hr />
+          <div className="py-3 text-gray-500  flex justify-between items-center">
+            <p className="text-[12px]">Average delivery time: ---</p>
+            <p>3 days</p>
+          </div>
+
+          <div className="flex flex-col sm:flex-row gap-2">
+            <button
+              className="bg-primaryLightColor text-white px-4 py-2 w-full mt-4  rounded"
+              onClick={handleContinue}
+            >
+              Continue
+            </button>
+            <button
+              className="bg-gray-400 text-white px-4 py-2 w-full mt-4  rounded"
+              onClick={handleBack}
+            >
+              Back
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
