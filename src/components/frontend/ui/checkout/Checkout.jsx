ed3a3f74e682eaeb85e23fb0ea8846d1cb26c1e8
Saifulla Jubair <saifullajubair6@gmail.com>
2024-05-16 12:23:35 +0600

    payment page dynamic

--- src/components/frontend/ui/checkout/Checkout.jsx
+++ src/components/frontend/ui/checkout/Checkout.jsx
@@ -10,6 +10,7 @@ import Loader from "../../../../shared/loader/Loader";
 import { BASE_URL } from "../../../../utils/baseURL";
 import { useQuery } from "@tanstack/react-query";
 import { AuthContext } from "../../../../context/AuthProvider";
+import { Link } from "react-router-dom";
 
 export default function Checkout() {
   const { user: userData } = useContext(AuthContext);
@@ -28,7 +29,9 @@ export default function Checkout() {
   const cart = useSelector((state) => state.furnitureCart.products);
   const subTotal = useSelector((state) => state.furnitureCart.subtotal);
   const totalQuantity = useSelector((state) => state.furnitureCart.quantity);
-  const delivery_charge = useSelector((state) => state.furnitureCart.delivery_charge);
+  const delivery_charge = useSelector(
+    (state) => state.furnitureCart.delivery_charge
+  );
   const deliveryType = useSelector((state) => state.furnitureCart.deliveryType);
   const [activeStep, setActiveStep] = useState(1);
 
@@ -52,105 +55,167 @@ export default function Checkout() {
   }
 
   return (
-    <div className="es_container">
-      <div className="py-10">
-        {/* Step Navigation */}
-        <div className="flex justify-center flex-wrap">
-          {/* Step - 1 */}
-          <p
-            className={`md:text-xl sm:text-lg font-semibold pb-2 flex gap-1 items-center cursor-pointer  ${activeStep >= 1 ? "text-primaryDeepColor" : ""
-              }`}
-            onClick={() => handleStepClick(1)}
-          >
-            <span
-              className={`sm:w-6  sm:h-6 w-4 h-4 sm:text-[14px] text-[10px] flex items-center justify-center text-white rounded-full mr-1    ${activeStep >= 1 ? "bg-primaryDeepColor" : "bg-gray-400 "
-                } `}
-            >
-              1
-            </span>{" "}
-            Bag <BsCart className="sm:text-xl " />
-            <div
-              className={`md:w-12 w-4 h-[1px] sm:mr-4 mr-1.5  rounded-full ${activeStep >= 1 ? "bg-primaryDeepColor " : "bg-gray-500"
+    <>
+      {cart?.length > 0 ? (
+        <div className="es_container">
+          <div className="py-10">
+            {/* Step Navigation */}
+            <div className="flex justify-center flex-wrap">
+              {/* Step - 1 */}
+              <p
+                className={`md:text-xl sm:text-lg font-semibold pb-2 flex gap-1 items-center cursor-pointer  ${
+                  activeStep >= 1 ? "text-primaryDeepColor" : ""
                 }`}
-            ></div>
-          </p>
+                onClick={() => handleStepClick(1)}
+              >
+                <span
+                  className={`sm:w-6  sm:h-6 w-4 h-4 sm:text-[14px] text-[10px] flex items-center justify-center text-white rounded-full mr-1    ${
+                    activeStep >= 1 ? "bg-primaryDeepColor" : "bg-gray-400 "
+                  } `}
+                >
+                  1
+                </span>{" "}
+                Bag <BsCart className="sm:text-xl " />
+                <div
+                  className={`md:w-12 w-4 h-[1px] sm:mr-4 mr-1.5  rounded-full ${
+                    activeStep >= 1 ? "bg-primaryDeepColor " : "bg-gray-500"
+                  }`}
+                ></div>
+              </p>
 
-          {/* Step - 2 */}
-          <p
-            className={`md:text-xl sm:text-lg font-semibold pb-2 flex gap-1 items-center cursor-pointer  ${activeStep >= 2 ? "text-primaryDeepColor" : "text-gray-700"
-              }`}
-            onClick={() => handleStepClick(2)}
-          >
-            <span
-              className={`sm:w-6  sm:h-6 w-4 h-4 sm:text-[14px] text-[10px] flex items-center justify-center text-white rounded-full mr-1    ${activeStep >= 2 ? "bg-primaryDeepColor" : "bg-gray-400  "
-                } `}
-            >
-              2
-            </span>{" "}
-            Address <CiLocationOn className="sm:text-xl font-semibold " />
-            <div
-              className={`md:w-12 sm:w-6 w-4 h-[1px] sm:mr-4 mr-1.5  rounded-full ${activeStep >= 2 ? "bg-primaryDeepColor " : "bg-gray-500"
+              {/* Step - 2 */}
+              <p
+                className={`md:text-xl sm:text-lg font-semibold pb-2 flex gap-1 items-center cursor-pointer  ${
+                  activeStep >= 2 ? "text-primaryDeepColor" : "text-gray-700"
                 }`}
-            ></div>
-          </p>
-          {/* Step - 3 */}
-          <p
-            className={`md:text-xl sm:text-lg font-semibold pb-2 flex gap-1 items-center  cursor-pointer ${activeStep >= 3 ? "text-primaryDeepColor" : "text-gray-700"
-              }`}
-            onClick={() => handleStepClick(3)}
-          >
-            <span
-              className={`sm:w-6  sm:h-6 w-4 h-4 sm:text-[14px] text-[10px] flex items-center justify-center text-white rounded-full mr-1    ${activeStep >= 3 ? "bg-primaryDeepColor" : "bg-gray-400 "
-                } `}
-            >
-              3
-            </span>{" "}
-            Payment <CiCreditCard1 className="sm:text-xl" />
-          </p>
-        </div>
-        <div className="lg:mx-10 md:mx-4 mx-2">
-          <div className="flex flex-col md:flex-row gap-4 my-4  lg:gap-8  ">
-            {/* Left side condition aria */}
-            <div className="w-full md:w-[68%]">
-              <div className="flex justify-between items-center p-4 ">
-                <p className="md:text-xl  font-semibold   flex gap-2 items-center">
-                  <BsCart size={26} /> Shopping Bag Item {totalQuantity}
-                </p>
-                <p className="md:text-xl  font-semibold   flex gap-2 items-center">
-                  Total: {subTotal}৳
-                </p>
-              </div>
-              {/* Left side step 1 */}
-              {activeStep === 1 && (
-                <CartItems
-                  cart={cart}
-                  totalQuantity={totalQuantity}
-                  subTotal={subTotal}
-                />
-              )}
-              {/* Left side step 2 */}
-
-              {activeStep === 2 && <Address setSelectedDeliveryLocation={setSelectedDeliveryLocation} selectedDeliveryLocation={selectedDeliveryLocation} settingData={settingData} deliveryType={deliveryType} userData={userData} />}
-              {/* Left side step 3 */}
-              {activeStep === 3 && <Payment />}
+                onClick={() => handleStepClick(2)}
+              >
+                <span
+                  className={`sm:w-6  sm:h-6 w-4 h-4 sm:text-[14px] text-[10px] flex items-center justify-center text-white rounded-full mr-1    ${
+                    activeStep >= 2 ? "bg-primaryDeepColor" : "bg-gray-400  "
+                  } `}
+                >
+                  2
+                </span>{" "}
+                Address <CiLocationOn className="sm:text-xl font-semibold " />
+                <div
+                  className={`md:w-12 sm:w-6 w-4 h-[1px] sm:mr-4 mr-1.5  rounded-full ${
+                    activeStep >= 2 ? "bg-primaryDeepColor " : "bg-gray-500"
+                  }`}
+                ></div>
+              </p>
+              {/* Step - 3 */}
+              <p
+                className={`md:text-xl sm:text-lg font-semibold pb-2 flex gap-1 items-center  cursor-pointer ${
+                  activeStep >= 3 ? "text-primaryDeepColor" : "text-gray-700"
+                }`}
+                onClick={() => handleStepClick(3)}
+              >
+                <span
+                  className={`sm:w-6  sm:h-6 w-4 h-4 sm:text-[14px] text-[10px] flex items-center justify-center text-white rounded-full mr-1    ${
+                    activeStep >= 3 ? "bg-primaryDeepColor" : "bg-gray-400 "
+                  } `}
+                >
+                  3
+                </span>{" "}
+                Payment <CiCreditCard1 className="sm:text-xl" />
+              </p>
             </div>
+            <div className="lg:mx-10 md:mx-4 mx-2">
+              <div className="flex flex-col md:flex-row gap-4 my-4  lg:gap-8  ">
+                {/* Left side condition aria */}
+                <div className="w-full md:w-[68%]">
+                  <div className="flex justify-between items-center p-4 ">
+                    <p className="md:text-xl  font-semibold   flex gap-2 items-center">
+                      <BsCart size={26} /> Shopping Bag Item {totalQuantity}
+                    </p>
+                    <p className="md:text-xl  font-semibold   flex gap-2 items-center">
+                      Total: {subTotal}৳
+                    </p>
+                  </div>
+                  {/* Left side step 1 */}
+                  {activeStep === 1 && (
+                    <CartItems
+                      cart={cart}
+                      totalQuantity={totalQuantity}
+                      subTotal={subTotal}
+                    />
+                  )}
+                  {/* Left side step 2 */}
 
-            {/* Right side  */}
+                  {activeStep === 2 && (
+                    <Address
+                      setSelectedDeliveryLocation={setSelectedDeliveryLocation}
+                      selectedDeliveryLocation={selectedDeliveryLocation}
+                      settingData={settingData}
+                      deliveryType={deliveryType}
+                      userData={userData}
+                    />
+                  )}
+                  {/* Left side step 3 */}
+                  {activeStep === 3 && <Payment />}
+                </div>
 
-            <RightSideAmount
-              subTotal={subTotal}
-              totalQuantity={totalQuantity}
-              handleContinue={handleContinue}
-              activeStep={activeStep}
-              handleBack={handleBack}
-              settingData={settingData}
-              delivery_charge={delivery_charge}
-              deliveryType={deliveryType}
-              selectedDeliveryLocation={selectedDeliveryLocation}
-            />
+                {/* Right side  */}
+
+                <RightSideAmount
+                  subTotal={subTotal}
+                  totalQuantity={totalQuantity}
+                  handleContinue={handleContinue}
+                  activeStep={activeStep}
+                  handleBack={handleBack}
+                  settingData={settingData}
+                  delivery_charge={delivery_charge}
+                  deliveryType={deliveryType}
+                  selectedDeliveryLocation={selectedDeliveryLocation}
+                />
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      ) : (
+        <>
+          <section className="flex items-center h-full sm:p-16 bg-gray-50 text-gray-800">
+            <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8 space-y-8 text-center sm:max-w-md">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"
+                className="w-40 h-40 text-gray-400"
+              >
+                <path
+                  fill="currentColor"
+                  d="M256,16C123.452,16,16,123.452,16,256S123.452,496,256,496,496,388.548,496,256,388.548,16,256,16ZM403.078,403.078a207.253,207.253,0,1,1,44.589-66.125A207.332,207.332,0,0,1,403.078,403.078Z"
+                ></path>
+                <rect
+                  width="176"
+                  height="32"
+                  x="168"
+                  y="320"
+                  fill="currentColor"
+                ></rect>
+                <polygon
+                  fill="currentColor"
+                  points="210.63 228.042 186.588 206.671 207.958 182.63 184.042 161.37 162.671 185.412 138.63 164.042 117.37 187.958 141.412 209.329 120.042 233.37 143.958 254.63 165.329 230.588 189.37 251.958 210.63 228.042"
+                ></polygon>
+                <polygon
+                  fill="currentColor"
+                  points="383.958 182.63 360.042 161.37 338.671 185.412 314.63 164.042 293.37 187.958 317.412 209.329 296.042 233.37 319.958 254.63 341.329 230.588 365.37 251.958 386.63 228.042 362.588 206.671 383.958 182.63"
+                ></polygon>
+              </svg>
+              <p className="text-3xl">
+                Looks like our product is currently unavailable!
+              </p>
+              <Link
+                to="/"
+                rel="noopener noreferrer"
+                className="px-8 py-3 font-semibold rounded bg-orange-600 text-gray-50 hover:scale-105 duration-100"
+              >
+                Back to homepage
+              </Link>
+            </div>
+          </section>
+        </>
+      )}
+    </>
   );
 }
--- src/components/frontend/ui/checkout/Payment.jsx
+++ src/components/frontend/ui/checkout/Payment.jsx
@@ -1,9 +1,152 @@
-export default function Payment() {
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+
+const Payment = () => {
+  const cart = useSelector((state) => state.furnitureCart.products);
+  const subTotal = useSelector((state) => state.furnitureCart.subtotal);
+  const totalQuantity = useSelector((state) => state.furnitureCart.quantity);
+  const delivery_charge = useSelector(
+    (state) => state.furnitureCart.delivery_charge
+  );
+  const deliveryType = useSelector((state) => state.furnitureCart.deliveryType);
+
+  const [partialPayProducts, setPartialPayProducts] = useState([]);
+  const [codProducts, setCodProducts] = useState([]);
+
+  useEffect(() => {
+    const partialPayAllProducts = cart?.filter(
+      (product) => product?.product_partial_payment === true
+    );
+    setPartialPayProducts(partialPayAllProducts);
+    const codAllProducts = cart?.filter(
+      (product) => product?.product_partial_payment === false
+    );
+    setCodProducts(codAllProducts);
+  }, [cart]);
+
+  console.log(partialPayProducts);
+  console.log(codProducts);
+
   return (
-    <div className=" bg-white border shadow rounded-lg  p-4">
-      <p className="text-3xl font-semibold pt-8 pb-2 flex gap-2 items-center">
-        This is step 3 Payment here
-      </p>
+    <div className=" bg-white border shadow rounded-lg  p-4 lg:p-8">
+      {partialPayProducts.length > 0 && (
+        <>
+          <p className="sm:text-xl font-semibold pt-3 pb-4   ">
+            Partial Payment
+          </p>
+
+          {partialPayProducts.map((product, index) => (
+            <div
+              className="border rounded mb-6 flex justify-between "
+              key={index}
+            >
+              <div className="flex  flex-col sm:flex-row md:px-0 px-4  gap-4">
+                <img
+                  className="md:w-36 md:h-36 w-20 h-20  rounded-l"
+                  src={product?.product_thumbnail}
+                  alt=""
+                />
+                <div className="py-1 md:px-4 space-y-[2px] ">
+                  <p className="md:text-xl text-sm font-semibold text-gray-600 ">
+                    {product?.product_name}
+                  </p>
+                  <p className="">Color: {product?.color}</p>
+                  <p className="">Price: {product?.price}</p>
+                  {product?.size && <p className="">Size: {product?.size}</p>}
+                  {product?.product_partial_payment &&
+                    product?.product_partial_payment == true && (
+                      <p className="">
+                        Partial Payment:{" "}
+                        {product?.product_partial_payment_amount}
+                      </p>
+                    )}
+                </div>
+              </div>
+              <dir className="flex flex-col md:px-0 space-y-2 m-3 ">
+                {/* quantity */}
+
+                <p className="font-semibold text-sm text-gray-800">
+                  Quantity: {product?.quantity}
+                </p>
+
+                <p>Sub Total: {product?.quantity * product?.price} ৳</p>
+              </dir>
+            </div>
+          ))}
+
+          <div className="flex justify-end flex-wrap gap-4 items-center">
+            <p className="text-error-300 font-semibold text-[12px]">
+              *Note: If you want to buy only this products in partial payment{" "}
+            </p>
+            <button className="btn text-white font-semibold  hover:bg-primaryLightColor/75 hover:scale-105 bg-primaryLightColor rounded px-2.5 py-1.5 duration-300">
+              Payment
+            </button>
+          </div>
+        </>
+      )}
+      {codProducts.length > 0 && (
+        <>
+          <p className="sm:text-xl font-semibold pt-3 pb-4   ">
+            Partial Payment
+          </p>
+
+          {codProducts.map((product, index) => (
+            <div
+              className="border rounded mb-6 flex justify-between "
+              key={index}
+            >
+              <div className="flex  flex-col sm:flex-row md:px-0 px-4  gap-4">
+                <img
+                  className="md:w-36 md:h-36 w-20 h-20  rounded-l"
+                  src={product?.product_thumbnail}
+                  alt=""
+                />
+                <div className="py-1 md:px-4 space-y-[2px] ">
+                  <p className="md:text-xl text-sm font-semibold text-gray-600 ">
+                    {product?.product_name}
+                  </p>
+                  <p className="">Color: {product?.color}</p>
+                  <p className="">Price: {product?.price}</p>
+                  {product?.size && <p className="">Size: {product?.size}</p>}
+                  {product?.product_partial_payment &&
+                    product?.product_partial_payment == true && (
+                      <p className="">
+                        Partial Payment:{" "}
+                        {product?.product_partial_payment_amount}
+                      </p>
+                    )}
+                </div>
+              </div>
+              <dir className="flex flex-col md:px-0 space-y-2 m-3 ">
+                {/* quantity */}
+
+                <p className="font-semibold text-sm text-gray-800">
+                  Quantity: {product?.quantity}
+                </p>
+
+                <p>Sub Total: {product?.quantity * product?.price} ৳</p>
+              </dir>
+            </div>
+          ))}
+
+          <div className="flex justify-end flex-wrap gap-4 items-center">
+            <p className="text-error-300 font-semibold text-[12px]">
+              *Note: If you want to buy only this products in COD payment{" "}
+            </p>
+            <button className="btn text-white font-semibold  hover:bg-primaryLightColor/75 hover:scale-105 bg-primaryLightColor rounded px-2.5 py-1.5 duration-300">
+              Order Now
+            </button>
+          </div>
+        </>
+      )}
+
+      <div className="w-full flex justify-center items-center gap-4 mt-10 mb-4">
+        <button className="btn text-white font-semibold  hover:bg-primaryLightColor/75 hover:scale-105 bg-primaryLightColor rounded px-2.5 py-1.5 duration-300">
+          Payment All
+        </button>
+      </div>
     </div>
   );
-}
+};
+
+export default Payment;
