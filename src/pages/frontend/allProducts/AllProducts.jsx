d6843a660a83c66f84e4ee101ea8660e60aa3777
Saifulla Jubair <saifullajubair6@gmail.com>
2024-05-21 18:19:03 +0600

    Add Products page

--- src/pages/frontend/allProducts/AllProducts.jsx
+++ src/pages/frontend/allProducts/AllProducts.jsx
@@ -11,6 +11,8 @@ import ProductCardSkeleton from "../../../shared/loader/ProductCardSkeleton";
 import ProductNotFound from "../../../components/common/productNotFound/ProductNotFound";
 import PriceRangeSlider from "./PriceRangeSlider";
 import { RxReload } from "react-icons/rx";
+import FrontPagination from "./FrontPagination";
+import MiniSpinner from "../../../shared/loader/MiniSpinner";
 
 export default function AllProducts() {
   const [mobileFiltersOpen, setMobileFiltersOpen] = useState(false);
@@ -24,6 +26,10 @@ export default function AllProducts() {
   const [loading, setLoading] = useState(false);
   const [minPrice, setMinPrice] = useState(1);
   const [maxPrice, setMaxPrice] = useState(200000);
+  // const [maxPriceRange, setMaxPriceRange] = useState(200000);
+  const [totalData, setTotalData] = useState(0);
+  const [rows, setRows] = useState(20);
+  const [page, setPage] = useState(1);
 
   const navigate = useNavigate();
   const { data: categories } = useGetCategoryQuery(undefined);
@@ -47,21 +53,22 @@ export default function AllProducts() {
     fetch(
       `${BASE_URL}/product${
         queryString.length > 0
-          ? `/filter_product?${queryString}`
-          : "/filter_product"
+          ? `/filter_product?${queryString}&page=${page}&limit=${rows}`
+          : `/filter_product?page=${page}&limit=${rows}`
       }`
     )
       .then((res) => res.json())
       .then((data) => {
         setProducts(data?.data?.products);
         setDefaultProducts(data?.data?.products);
-        setMaxPrice(data?.data?.maxPriceRange);
+        // setMaxPriceRange(data?.data?.maxPriceRange);
+        setTotalData(data?.data?.totalData);
         setLoading(false);
       })
       .catch((error) => console.error(error));
 
     setMobileFiltersOpen(false);
-  }, [queryParameters]);
+  }, [queryParameters, page, rows]);
 
   const handlePriceChange = ([min, max]) => {
     setMinPrice(min);
@@ -139,6 +146,9 @@ export default function AllProducts() {
     };
   }, []);
 
+  const startIndex = (page - 1) * rows + 1;
+  const endIndex = startIndex + Number(rows) - 1;
+
   let gridCols = "grid-cols-1";
   if (windowWidth >= 440) gridCols = "grid-cols-2";
   if (windowWidth >= 768) gridCols = "md:grid-cols-3";
@@ -160,29 +170,31 @@ export default function AllProducts() {
                 className="flex items-center sm:w-auto w-full justify-between sm:justify-center flex-wrap
                gap-4"
               >
-                <div className="flex gap-2 items-center ">
+                <div className="flex gap-2 items-center justify-center ">
                   <span
                     className="text-gray-800 mr-2 items-center gap-2 flex bg-gray-100 p-2 rounded lg:hidden"
                     onClick={() => setMobileFiltersOpen(!mobileFiltersOpen)}
                   >
                     <IoFilterOutline size={20} /> <span>Filter</span>
                   </span>
-                  {queryParameters.size > 0 && (
-                    <li>
-                      <span
-                        className="hidden bg-gray-100  shadow p-1 rounded sm:block"
-                        onClick={handleResetFilter}
-                      >
-                        Reset Filter
-                      </span>
-                      <span
-                        onClick={handleResetFilter}
-                        className="block sm:hidden"
-                      >
-                        <RxReload />
-                      </span>
-                    </li>
-                  )}
+                  <span>
+                    {queryParameters.size > 0 && (
+                      <>
+                        <span
+                          className="hidden bg-gray-100  shadow p-1 rounded sm:block"
+                          onClick={handleResetFilter}
+                        >
+                          Reset Filter
+                        </span>
+                        <span
+                          onClick={handleResetFilter}
+                          className="block sm:hidden"
+                        >
+                          <RxReload />
+                        </span>
+                      </>
+                    )}
+                  </span>
                 </div>
                 <div className="flex items-center justify-center gap-2">
                   <p className="hidden sm:flex">Sort By </p>
@@ -204,6 +216,7 @@ export default function AllProducts() {
               </div>
             </div>
           </div>
+          {/* <div className="p-4 bg-white sm:px-6 rounded my-2"></div> */}
 
           <div className="grid grid-cols-12 gap-4">
             <div className="lg:col-span-3 bg-white rounded xl:mr-16 lg:mr-10 px-6 py-4 my-6 hidden lg:block">
@@ -211,9 +224,19 @@ export default function AllProducts() {
                 <h3 className="text-[16px] font-semibold py-2 text-gray-700">
                   Price Range
                 </h3>
+                {/* {loading ? (
+                  <MiniSpinner />
+                ) : (
+                  <PriceRangeSlider
+                    maxPrice={maxPrice}
+                    minPrice={minPrice}
+                    onChange={handlePriceChange}
+                  />
+                )} */}
+
                 <PriceRangeSlider
                   minPrice={1}
-                  maxPrice={20000}
+                  maxPrice={200000}
                   onPriceChange={handlePriceChange}
                 />
               </div>
@@ -290,7 +313,7 @@ export default function AllProducts() {
                 )}
               </div>
             </div>
-            <div className="lg:col-span-9 col-span-12 max-h-screen overflow-y-scroll scrollbar-hide pr-2">
+            <div className="lg:col-span-9 col-span-12 pr-2">
               {loading ? (
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 my-4">
                   <ProductCardSkeleton />
@@ -313,6 +336,29 @@ export default function AllProducts() {
                   )}
                 </div>
               )}
+              {totalData > 0 && (
+                <div className="flex justify-between items-center py-5">
+                  {/* Pagination */}
+                  <FrontPagination
+                    rows={rows}
+                    page={page}
+                    setPage={setPage}
+                    setRows={setRows}
+                    totalData={totalData}
+                  />
+                  <div className="text-xs">
+                    <span className="mr-1 font-semibold text-primary">
+                      Showing
+                    </span>
+                    <span className="font-medium text-gray-700 text-xs mr-1">
+                      {startIndex === 0 ? 1 : startIndex} -{" "}
+                      {endIndex > totalData ? totalData : endIndex}
+                    </span>
+                    of {totalData > 0 ? totalData : 0}{" "}
+                    {totalData > 1 ? "records" : "record"}
+                  </div>
+                </div>
+              )}
             </div>
           </div>
         </section>
@@ -321,26 +367,33 @@ export default function AllProducts() {
           <>
             <div
               ref={sidebarRef}
-              className={`fixed inset-0 z-40 w-64 min-h-screen bg-white transition-transform duration-500 transform ease-in-out ${
+              className={`fixed inset-0 z-50 w-64 min-h-screen bg-white transition-transform duration-500 transform ease-in-out overflow-y-auto scrollbar-thin px-4 ${
                 mobileFiltersOpen ? "translate-x-0" : "-translate-x-full"
               } lg:hidden`}
             >
-              <span className="flex justify-end px-4 py-4">
+              <span className="flex justify-end  py-4">
                 <IoClose
                   size={24}
                   onClick={() => setMobileFiltersOpen(!mobileFiltersOpen)}
                 />
               </span>
-              <div className="bg-white px-4 pt-2 pb-4">
-                <div className="mb-4">
-                  <span className="flex justify-between items-center">
-                    <h3 className="text-[16px] font-semibold py-2 text-gray-700">
-                      Categories
-                    </h3>
+
+              <div className="mb-4">
+                <span
+                  className="flex justify-between items-center cursor-pointer"
+                  onClick={() => setCategoryShow(!categoryShow)}
+                >
+                  <h3 className="text-[16px] font-semibold py-2 text-gray-700">
+                    Categories
+                  </h3>
+                  <span className="text-gray-700">
+                    {categoryShow ? <FaAngleDown /> : <FaAngleUp />}
                   </span>
+                </span>
+                {categoryShow && (
                   <ul
                     role="list"
-                    className="space-y-4 pl-2 pb-6 text-sm font-medium text-gray-600 max-h-[360px] overflow-y-auto scrollbar-thin"
+                    className="space-y-4 pl-2 pb-6 text-sm font-medium text-gray-600 "
                   >
                     {categories?.data?.map((category) => (
                       <li key={category?.category_name}>
@@ -348,14 +401,56 @@ export default function AllProducts() {
                           onClick={() =>
                             handleCategoryClick(category?.category_slug)
                           }
-                          className="hover:text-primaryLightColor duration-200"
+                          className={`${
+                            selectedCategory === category?.category_slug
+                              ? "text-primaryLightColor"
+                              : " "
+                          } hover:text-primaryLightColor duration-200`}
                         >
                           {category?.category_name}
                         </button>
                       </li>
                     ))}
                   </ul>
-                </div>
+                )}
+              </div>
+              <hr />
+
+              <div className="my-4">
+                <span
+                  className="flex justify-between items-center "
+                  onClick={() => setColorShow(!colorShow)}
+                >
+                  <h3 className="text-[16px] font-semibold py-2 text-gray-700 ">
+                    Color
+                  </h3>
+                  <span className="text-gray-700">
+                    {colorShow ? <FaAngleDown /> : <FaAngleUp />}
+                  </span>
+                </span>
+                {colorShow && (
+                  <ul
+                    role="list"
+                    className="space-y-4 pl-2 pb-6 text-sm font-medium text-gray-600"
+                  >
+                    {colors?.data?.map((color) => (
+                      <li key={color?.color_name}>
+                        <button
+                          onClick={() => {
+                            handleColorsClick(color?.color_name);
+                          }}
+                          className={`${
+                            selectedColor === color?.color_name
+                              ? "text-primaryLightColor"
+                              : " "
+                          } hover:text-primaryLightColor duration-200`}
+                        >
+                          {color?.color_name}
+                        </button>
+                      </li>
+                    ))}
+                  </ul>
+                )}
               </div>
             </div>
             <div
