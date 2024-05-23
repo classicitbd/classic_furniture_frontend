5f81519e7bdda9a93ec9659b531bba9e63fa4baf
Hasiful Islam <nazmulbhuyian000@gmail.com>
2024-05-23 12:34:45 +0600

    done

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,2 +1,3 @@
-export const BASE_URL = "http://localhost:5000/api/v1";
-// export const BASE_URL = "https://walrus-app-btp85.ondigitalocean.app/api/v1";
+// export const BASE_URL = "http://localhost:5000/api/v1";
+export const BASE_URL =
+  "https://furniture-backend-tydip.ondigitalocean.app/api/v1";
