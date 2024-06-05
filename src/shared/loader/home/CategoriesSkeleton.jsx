9d51f9a1cef4426d44d2b0ac49a9d4bd41e96f7d
Saifulla Jubair <saifullajubair6@gmail.com>
2024-06-05 09:58:49 +0600

    bottom header categories loading skeleton done

---
+++ src/shared/loader/home/CategoriesSkeleton.jsx
@@ -0,0 +1,17 @@
+const CategoriesSkeleton = () => {
+  return (
+    <div className="">
+      <div className=" ">
+        <div className="flex space-x-10">
+          <div className="bg-gray-300 h-6 w-20 animate-pulse"></div>
+          <div className="bg-gray-300 h-6 w-20 animate-pulse"></div>
+          <div className="bg-gray-300 h-6 w-20 animate-pulse"></div>
+          <div className="bg-gray-300 h-6 w-20 animate-pulse"></div>
+          <div className="bg-gray-300 h-6 w-20 animate-pulse"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CategoriesSkeleton;
