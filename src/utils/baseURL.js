89e45d5f72dc6952c2232c719aaa260e9adb637a
nazmulbhuyian <nazmulbhuyian000@gmail.com>
2024-07-10 13:14:09 +0600

    done

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,3 +1,3 @@
 // export const BASE_URL = "http://localhost:5000/api/v1";
 export const BASE_URL =
-  "https://furniture-backend-2zc4j.ondigitalocean.app/api/v1";
+  "https://furniture-backend-245ct.ondigitalocean.app/api/v1";
